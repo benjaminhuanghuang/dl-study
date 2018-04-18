6a37d0738a9d08721af6b74f554459ffe9c73002
Benjamin Huang <bhuang@rms.com>
2018-04-18 13:25:30 -0700

    update

--- mnist.py
+++ mnist.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
-# A one-hot vector is a vector which is 0 in most dimensions, and 1 in a single dimension
+# Read data
+# one-hot vector is a vector which is 0 in most dimensions, and 1 in a single dimension
 # For example, 3 would be [0,0,0,1,0,0,0,0,0,0]
 mnist = input_data.read_data_sets('./data/MNIST', one_hot=True)
-
 
 x = tf.placeholder(dtype=tf.float32, shape=[None, 784], name='x')
 # mnist.train.labels is a [55000, 10] array of floats
@@ -19,10 +19,9 @@ batch_size = 1000
 
 
 def add_layer(input_data, input_num, output_num, activation_fun=None):
+    w = tf.Variable(initial_value=tf.random_normal(shape=[input_num, output_num]))
+    b = tf.Variable(initial_value=tf.random_normal(shape=[1, output_num]))
     # output = input_data * weight + bias
-    w = tf.Variable(initial_value=tf.random_normal(
-        shape=[input_num, output_num]))
-    b = tf.Variable(initial_value=tf.random_normal(shape=[i, output_num]))
     output = tf.add(tf.matmul(input_data, w), b)
 
     if activation_fun:
@@ -39,16 +38,18 @@ def build_nn(data):
 
 def train_nn(data):
     output = build_nn(data)
-    loss = tf.nn.softmax_cross_entropy_with_logits(locals=y, logits=output)
+    loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output)
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=1).minimize(loss)
-    for i in range(50):
-        epoch_cost = 0
-        for _ in range(int(mnist.train.num_examples / batch_size)):
-            x_data, y_data = mnist.train.next_batch(batch_size)
-            cost, _ = sess.run([loss, optimizer], feed_dict={x: x_data, y: y_data})
-            epoch_cost += cost
-    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(output, 1)), tf.float32))
-    acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(50):
+            epoch_cost = 0
+            for _ in range(int(mnist.train.num_examples / batch_size)):
+                x_data, y_data = mnist.train.next_batch(batch_size)
+                cost, _ = sess.run([loss, optimizer], feed_dict={x: x_data, y: y_data})
+                epoch_cost += cost
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(y, 1), tf.argmax(output, 1)), tf.float32))
+        acc = sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels})
 
 
 train_nn(x)
